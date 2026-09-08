f2174c0a074cf182252aa0ebea60d0b8a8dee08d
Srijan Bhatia <srijanbhatia1998@gmail.com>
2026-09-08 21:32:44 +0530

    Project initialization and docker setup

---
+++ src/main/java/com/parseforge/parseforge/ParseforgeApplication.java
@@ -0,0 +1,13 @@
+package com.parseforge.parseforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParseforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParseforgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parseforge/parseforge/ParseforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parseforge.parseforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParseforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
